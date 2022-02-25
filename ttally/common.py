f0f37f7a2a21fb09f65b355d9f583e727f1f3783
Sean Breckenridge <seanbrecke@gmail.com>
2022-02-24 18:51:40 -0800

    use resolve_annotation_single

--- ttally/common.py
+++ ttally/common.py
@@ -14,16 +14,11 @@ def namedtuple_extract_from_annotation(nt: Type[NamedTuple], _type: Any) -> str:
     'something'
     """
     import inspect
-    from autotui.typehelpers import get_union_args
+    from autotui.typehelpers import resolve_annotation_single
 
     for attr_name, param in inspect.signature(nt).parameters.items():
-        attr_type = param.annotation
         # Optional[(<class 'int'>, False)]
-        res = get_union_args(attr_type)
-        if res := get_union_args(attr_type):
-            attr_types, _ = res
-            assert len(attr_types) == 1
-            attr_type = attr_types[0]
+        attr_type, _ = resolve_annotation_single(param.annotation)
 
         if attr_type == _type:
             return attr_name
