7fa3c6ebe720659a1d347b414ad22d6022da3488
Sean Breckenridge <seanbrecke@gmail.com>
2022-11-22 00:11:21 -0800

    remove unused imports

--- ttally/cache.py
+++ ttally/cache.py
@@ -1,7 +1,7 @@
 import os
 import json
 from pathlib import Path
-from typing import Dict, Optional, Set, Any, NamedTuple, Iterator, List
+from typing import Dict, Optional, Set, Any, List
 import shelve
 
 from .models import MODELS
--- ttally/codegen.py
+++ ttally/codegen.py
@@ -6,7 +6,7 @@ from .models import MODELS
 def generate_shell_aliases(python_loc: str = "python3") -> Iterator[str]:
     pre = f"'{python_loc} -m ttally "
     suf = "'"
-    for mname, model in MODELS.items():
+    for mname in MODELS.keys():
         yield f"alias {mname}={pre}prompt {mname}{suf}"
         yield f"alias {mname}-now={pre}prompt-now {mname}{suf}"
         yield f"alias {mname}-recent={pre}recent {mname}{suf}"
